348253b4eacca26da1ca6d7aa5a9c8baaa38c700
Federico Dondi <federico.dondi2@studio.unibo.it>
2021-01-29 20:12:56 +0100

    feat: add wc_update_role_name.php

---
+++ wc_update_role_name.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* visually rename a WooCommerce user role
+*
+* @param  	  string  $role     the role to visually rename.
+* @param      string  $new_name the renaming value.
+* @return     bool - true if renaming was successful, false otherwise
+* @author     Federico Dondi
+* @copyright	Federico Dondi - 29/01/2021
+* @version    1.0
+*/
+function wc_update_role_name($role, $new_name)
+{
+  if ($role != 'shop_manager' ||
+      $role != 'customer')
+      return false;
+
+  global $wp_roles;
+
+	if (isset($wp_roles) == false)
+            $wp_roles = new WP_Roles();
+            
+  $wp_roles->roles[$role]['name'] = $new_name;
+  $wp_roles->role_names[$role] = $new_name;
+
+  return true;
+}
+
+add_action('init', function()
+{
+  wc_update_role_name('shop_manager', 'WooCommerce Administrator');
+  wc_update_role_name('customer', 'WooCommerce Customer');
+});
+?>
