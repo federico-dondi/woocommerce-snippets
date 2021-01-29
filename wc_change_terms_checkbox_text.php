160f398256a3497616c237f4e5a3fd0788226830
Federico Dondi <federico.dondi2@studio.unibo.it>
2021-01-29 20:23:57 +0100

    fix: extract function

--- wc_change_terms_checkbox_text.php
+++ wc_change_terms_checkbox_text.php
@@ -8,8 +8,10 @@
 * @copyright	Federico Dondi - 29/01/2021
 * @version    1.0
 */
-add_filter('woocommerce_get_terms_and_conditions_checkbox_text', function($text)
+function wc_change_terms_checkbox_text($text)
 {
   return 'Your custom T&C disclaimer.';
-});
+}
+
+add_filter('woocommerce_get_terms_and_conditions_checkbox_text', 'wc_change_terms_checkbox_text', 10, 1);
 ?>
